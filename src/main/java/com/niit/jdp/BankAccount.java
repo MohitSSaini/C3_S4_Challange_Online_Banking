9a450bd9131e1872deffc89676de4f788d70b385
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-30 01:16:45 +0530

    Create a attributes and variable in BankAccount.java class

---
+++ src/main/java/com/niit/jdp/BankAccount.java
@@ -0,0 +1,19 @@
+/*
+ * Author Name: Mohit Saini
+ * Date: 29-08-2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp;
+
+public class BankAccount {
+    private String accountName;
+    private long accountNumber;
+    private double availableBalance;
+    private String openingDate;
+    private String modeOfOperation;
+    private String internetBanking;
+    private double interestRate;
+    private double totalBalance;
+    
+    
+}
