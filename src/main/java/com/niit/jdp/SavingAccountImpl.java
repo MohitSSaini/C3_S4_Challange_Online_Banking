5738608c811ef6e0c0f8a536c395e9768bbecf10
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-30 01:30:27 +0530

    define a object and variable in SavingAccountImpl.java class

---
+++ src/main/java/com/niit/jdp/SavingAccountImpl.java
@@ -0,0 +1,17 @@
+/*
+ * Author Name: Mohit Saini
+ * Date: 29-08-2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp;
+
+public class SavingAccountImpl {
+    public static void main ( String[] args ) {
+        SavingAccount savingAccount = new SavingAccount ( "Mohit" ,
+                12342233 , 14538.56 ,
+                "12/07/2019" , "JointAccount" ,
+                "Active" , 10.5 , 50000.0 );
+        
+        
+    }
+}
