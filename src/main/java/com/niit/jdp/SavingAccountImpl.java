b507eb7be620f46ab8cc43634664cc2fe811916e
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-30 01:31:21 +0530

    print all  SavingAccountImpl.java class

--- src/main/java/com/niit/jdp/SavingAccountImpl.java
+++ src/main/java/com/niit/jdp/SavingAccountImpl.java
@@ -11,7 +11,18 @@ public class SavingAccountImpl {
                 12342233 , 14538.56 ,
                 "12/07/2019" , "JointAccount" ,
                 "Active" , 10.5 , 50000.0 );
-        
-        
+        System.out.println ( "HolderName = " + savingAccount.getAccountName ( ) );
+        System.out.println ( "accountNumber = " + savingAccount.getAccountNumber ( ) );
+        System.out.println ( "Avalable balance = " + savingAccount.getAvailableBalance ( ) );
+        System.out.println ( "openingdatev =" + savingAccount.getOpeningDate ( ) );
+        System.out.println ( "mode = " + savingAccount.getModeOfOperation ( ) );
+        System.out.println ( "banking = " + savingAccount.getInternetBanking ( ) );
+        System.out.println ( "rate = " + savingAccount.getInterestRate ( ) );
+        double totalBalance = savingAccount.getRetrieveBalance ( 50000.0 );
+        double totalBalanceAfterDebit = savingAccount.getDebitBalance ( 50000.0 , 10000.0 );
+        double totalBalanceCredit = savingAccount.getCreditBalance ( 40000.0 , 40000.0 );
+        System.out.println ( "totalBalance = " + totalBalance );
+        System.out.println ( "totalBalanceAfterDebit = " + totalBalanceAfterDebit );
+        System.out.println ( "totalBalanceCredit = " + totalBalanceCredit );
     }
 }
